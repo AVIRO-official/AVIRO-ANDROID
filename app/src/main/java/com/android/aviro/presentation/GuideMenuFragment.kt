bf0024a7f420a31b094c827f64af8dafed529127
honor-sky <keung903@naver.com>
2023-12-12 00:22:46 +0900

    feat : 앱 초기 가이드라인 안내 기능 추가

---
+++ app/src/main/java/com/android/aviro/presentation/GuideMenuFragment.kt
@@ -0,0 +1,36 @@
+package com.android.aviro.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android.aviro.databinding.FragmentGuideMenuBinding
+
+class GuideMenuFragment : Fragment() {
+
+    private var _binding: FragmentGuideMenuBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentGuideMenuBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
