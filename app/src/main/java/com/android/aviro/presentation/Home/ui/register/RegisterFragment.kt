add5ea1515c8aae81b70f26097a017ef5c8eb429
honor-sky <keung903@naver.com>
2023-12-12 00:53:07 +0900

    feat : 메인 화면(홈화면) - '가게 등록하기' 기능 추가

---
+++ app/src/main/java/com/android/aviro/presentation/Home/ui/register/RegisterFragment.kt
@@ -0,0 +1,40 @@
+package com.android.aviro.presentation.Home.ui.register
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.android.aviro.databinding.FragmentRegisterBinding
+
+
+class RegisterFragment : Fragment() {
+
+    private var _binding: FragmentRegisterBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val registerViewModel =
+            ViewModelProvider(this).get(RegisterViewModel::class.java)
+
+        _binding = FragmentRegisterBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
