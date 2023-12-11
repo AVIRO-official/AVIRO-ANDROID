b5827e888a13fc4c4384efd4bc827b68fe94feeb
honor-sky <keung903@naver.com>
2023-12-12 00:56:41 +0900

    feat : 회원가입, 로그인 기능에 사용될 User 정보 data class 추가

---
+++ app/src/main/java/com/android/aviro/presentation/Entity/User.kt
@@ -0,0 +1,18 @@
+package com.android.aviro.presentation.Entity
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class User (
+
+    @SerializedName("photoSpotId")
+    val userId : Int,
+    val userName : String,
+    val userEmail : String,
+    var nickname : String,
+    var birthday : Int?,
+    var gender : String?, //ENUM ('female', 'male', 'other')
+    var marketingAgree : Boolean
+)
