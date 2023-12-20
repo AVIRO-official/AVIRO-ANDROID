3d747ae5f05bc81fcfeb0cf153dd514a9750289e
honor-sky <keung903@naver.com>
2023-12-20 22:01:26 +0900

    feat : 뷰모델 팩토리 추가 (뷰모델에 의존성 주입을 하면서 매개변수가 있는 뷰모델 생성을 위해 사용)

--- app/src/main/java/com/android/aviro/presentation/ViewModelFactory.kt
+++ app/src/main/java/com/android/aviro/presentation/ViewModelFactory.kt
@@ -1,4 +1,19 @@
 package com.android.aviro.presentation
 
-class ViewModelFactory {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.android.aviro.domain.usecase.user.CreateSocialAccountUseCase
+import com.android.aviro.domain.usecase.user.CreateUserUseCase
+import com.android.aviro.presentation.sign.SignViewModel
+
+class ViewModelFactory(val createSocialAccountUseCase: CreateSocialAccountUseCase,
+                       val createUserUseCase: CreateUserUseCase
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return if (modelClass.isAssignableFrom(SignViewModel::class.java)) {
+            SignViewModel(createSocialAccountUseCase,createUserUseCase) as T
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
 }
