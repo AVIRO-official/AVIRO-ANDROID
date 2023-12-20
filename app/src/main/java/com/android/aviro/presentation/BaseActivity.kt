4df98f5f8c79e64ece4ed8bba725ecc4bdbf57f5
honor-sky <keung903@naver.com>
2023-12-20 22:00:33 +0900

    feat : 레이아웃 뷰바인딩, 라이프사이클 설정을 기본으로 해주는 액티비티 베이스 코드 추가

--- app/src/main/java/com/android/aviro/presentation/BaseActivity.kt
+++ app/src/main/java/com/android/aviro/presentation/BaseActivity.kt
@@ -1,4 +1,28 @@
 package com.android.aviro.presentation
 
-class BaseActivity {
+import android.os.Bundle
+import android.view.View.inflate
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+
+abstract class BaseActivity<V : ViewDataBinding>(
+    @LayoutRes private val layoutId: Int
+) : AppCompatActivity() {
+
+    protected lateinit var binding : V
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+
+    }
+
+    override fun onDestroy() {
+        binding.unbind()
+        super.onDestroy()
+    }
 }
