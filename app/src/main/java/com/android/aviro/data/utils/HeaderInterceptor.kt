c0a00f6261c3c160a894e7b1d63fdc0ff1ee0482
honor-sky <keung903@naver.com>
2024-01-09 12:03:56 +0900

    feat : 서버 요청시 헤더 추가 인터셉터 생성

--- app/src/main/java/com/android/aviro/data/utils/HeaderInterceptor.kt
+++ app/src/main/java/com/android/aviro/data/utils/HeaderInterceptor.kt
@@ -1,4 +1,16 @@
 package com.android.aviro.data.utils
 
-class HeaderInterceptor {
+import com.android.aviro.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class HeaderInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response { //retrofit2, okhttp3
+        val builder = chain.request().newBuilder()
+        builder.addHeader("Header", "${BuildConfig.AWS_API_HEADER}")
+
+        return chain.proceed(builder.build())
+    }
+
 }
