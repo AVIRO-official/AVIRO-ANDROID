4f8147bf76dceb66870cca7a6b93ac403e0dff0e
honor-sky <keung903@naver.com>
2024-01-09 11:54:20 +0900

    feat : di 추가

--- app/src/main/java/com/android/aviro/data/di/DataSourceModule.kt
+++ app/src/main/java/com/android/aviro/data/di/DataSourceModule.kt
@@ -1,24 +1,39 @@
 package com.android.aviro.data.di
 
-import com.android.aviro.data.datasource.AuthDataSource
-import com.android.aviro.data.datasource.AuthDataSourceImp
-import com.android.aviro.data.repository.AuthRepositoryImp
-import com.android.aviro.domain.repository.AuthRepository
+import com.android.aviro.data.datasource.auth.AuthDataSource
+import com.android.aviro.data.datasource.auth.AuthDataSourceImp
+import com.android.aviro.data.datasource.datastore.DataStoreDataSource
+import com.android.aviro.data.datasource.datastore.DataStoreDataSourceImp
+import com.android.aviro.data.datasource.member.MemberDataSource
+import com.android.aviro.data.datasource.member.MemberDataSourceImp
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RemoteModule {
+abstract class DataSourceModule {
 
-        @Binds
-        @Singleton
-        abstract fun bindsAuthDataSource(
-            repository: AuthDataSourceImp,
-        ): AuthDataSource
+    @Binds
+    @Singleton
+    abstract fun bindsAuthDataSource(
+        auth_datasource: AuthDataSourceImp,
+    ): AuthDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindsDataStoreDataSource(
+        datastore_datasource: DataStoreDataSourceImp,
+    ): DataStoreDataSource
+
+
+    @Binds
+    @Singleton
+    abstract fun bindsMemberDataSource(
+        member_datasource: MemberDataSourceImp,
+    ): MemberDataSource
+
+
 }
--- app/src/main/java/com/android/aviro/data/di/RepositoryModule.kt
+++ app/src/main/java/com/android/aviro/data/di/RepositoryModule.kt
@@ -1,4 +1,34 @@
 package com.android.aviro.data.di
 
-class RepositoryModule {
+import com.android.aviro.data.repository.AuthRepositoryImp
+import com.android.aviro.data.repository.MemberRepositoryImp
+import com.android.aviro.domain.repository.AuthRepository
+import com.android.aviro.domain.repository.MemberRepository
+import dagger.Binds
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindsAuthRepository(
+        auth_repository: AuthRepositoryImp,
+    ): AuthRepository
+
+
+    @Binds
+    @Singleton
+    abstract fun bindsMemberRepository(
+       member_repository: MemberRepositoryImp,
+    ): MemberRepository
+
+
 }
