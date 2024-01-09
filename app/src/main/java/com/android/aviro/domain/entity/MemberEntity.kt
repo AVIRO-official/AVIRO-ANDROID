bcba37eaa720d0dda36d51b178c6feff7254ca90
honor-sky <keung903@naver.com>
2024-01-09 12:07:05 +0900

    feat : 도메인 엔티티 생성 - 도메인 레이어에서 객체 생성하는 경우 사용, 회원가입 멤버 정보 담을 entity 생성

--- app/src/main/java/com/android/aviro/domain/entity/MemberEntity.kt
+++ app/src/main/java/com/android/aviro/domain/entity/MemberEntity.kt
@@ -1,3 +1,31 @@
 package com.android.aviro.domain.entity
 
-data class MemberEntity()
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class MemberEntity(
+
+    @SerializedName("refreshToken")
+    val refreshToken : String,
+    @SerializedName("accessToken")
+    val accessToken : String,
+
+    @SerializedName("userId")
+    val userId : Int,
+    @SerializedName("userName")
+    val userName : String,
+    @SerializedName("userEmail")
+    val userEmail : String,
+    @SerializedName("nickname")
+    val nickname : String,
+    @SerializedName("birthday")
+    val birthday : Int?,
+    @SerializedName("gender")
+    val gender : String?,
+    @SerializedName("marketingAgree")
+    val marketingAgree : Boolean,
+
+    ) : Parcelable
