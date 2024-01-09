d9967bc899d35aa818c27e2a6263fe331e597341
honor-sky <keung903@naver.com>
2024-01-09 11:52:36 +0900

    feat :토큰 및 사용자 정보 저장하는 데이터스토어 생성

--- app/src/main/java/com/android/aviro/data/datasource/datastore/DataStoreDataSource.kt
+++ app/src/main/java/com/android/aviro/data/datasource/datastore/DataStoreDataSource.kt
@@ -1,4 +1,9 @@
 package com.android.aviro.data.datasource.datastore
 
 interface DataStoreDataSource {
+    suspend fun readDataStore(key: String): String?
+    suspend fun writeDataStore(key: String, value: String)
+    suspend fun removeDataStore(key: String)
+    suspend fun resetDataStore()
+
 }
