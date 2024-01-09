ca36ec876b38bcc5081be6a232ac6bab687e0a47
honor-sky <keung903@naver.com>
2024-01-09 12:05:48 +0900

    feat : 인증,멤버 관련  repository 인터페이스 생성

--- app/src/main/java/com/android/aviro/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/android/aviro/domain/repository/AuthRepository.kt
@@ -1,4 +1,13 @@
 package com.android.aviro.domain.repository
 
-class AuthRepository {
+import kotlinx.coroutines.flow.Flow
+
+interface AuthRepository {
+
+    suspend fun getTokensFromLocal() : Boolean// 토큰 생성 및 저장
+    suspend fun getTokensFromRemote(id_token : String, auth_code : String) : Result<Any>
+    suspend fun saveTokenToLocal(access_token : String, refresh_token : String)
+    suspend fun requestSignIn(token : String) : Boolean
+    suspend fun removeTokens()
+
 }
--- app/src/main/java/com/android/aviro/domain/repository/MemberRepository.kt
+++ app/src/main/java/com/android/aviro/domain/repository/MemberRepository.kt
@@ -1,4 +1,14 @@
 package com.android.aviro.domain.repository
 
-class MemberRepository {
+import com.android.aviro.data.entity.member.NicknameEntity
+import com.android.aviro.data.entity.member.NicknameCheckResponse
+
+interface MemberRepository {
+
+    suspend fun getMemberInfoFromLocal(key : String) : String?
+    suspend fun saveMemberInfoToLocal(user_id : Int, user_name : String, user_email : String, nickname : String)
+
+    suspend fun creatMember(nickname: String, birth : Int?, gender : String?, marketingAgree : Int) : Result<Any>
+    suspend fun checkNickname(nickname : NicknameEntity) :  Result<NicknameCheckResponse>
+
 }
