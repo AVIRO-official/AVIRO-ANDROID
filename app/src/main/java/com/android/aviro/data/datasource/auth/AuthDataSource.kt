e59a2bb65081c16eff2544a4183f8571910aeaae
honor-sky <keung903@naver.com>
2024-01-09 11:27:32 +0900

    feat : 자동로그인, 토큰 요청 관련 데이터소스 코드 추가(api 호출)

--- app/src/main/java/com/android/aviro/data/datasource/auth/AuthDataSource.kt
+++ app/src/main/java/com/android/aviro/data/datasource/auth/AuthDataSource.kt
@@ -1,16 +1,12 @@
-package com.android.aviro.data.datasource
+package com.android.aviro.data.datasource.auth
 
-import com.android.aviro.data.entity.auth.AuthRequestDTO
-import com.android.aviro.data.entity.auth.AuthResponseDTO
-import kotlinx.coroutines.flow.Flow
+import com.android.aviro.data.entity.auth.SignRequestDTO
+import com.android.aviro.data.entity.auth.TokensRequestDTO
 
-interface AuthDataSource {
+interface AuthDataSource { // 인증 // 인증 절차 수행 및 토큰 발급에 관여
 
-    suspend fun getTokens(request: AuthRequestDTO) : Result<Any> // local, remote로 부터 토큰 가져옴
-    suspend fun getTokens() : String
-    suspend fun removeTokens()
-    suspend fun updateTokens(accessToken: String, refreshToken: String, userId: String)
-    //fun saveToken(accessToken: String, refreshToken: String, userId: String)
-
+    suspend fun getTokens(request: TokensRequestDTO) : Result<Any> // remote로 부터 토큰 가져옴
+    suspend fun sign(request: SignRequestDTO) : Result<Any>
+    suspend fun removeTokens(refresh_token : String)
 
 }
--- app/src/main/java/com/android/aviro/data/datasource/auth/AuthDataSourceImp.kt
+++ app/src/main/java/com/android/aviro/data/datasource/auth/AuthDataSourceImp.kt
@@ -1,28 +1,23 @@
-package com.android.aviro.data.datasource
+package com.android.aviro.data.datasource.auth
 
-import android.content.Context
-import android.util.Log
-import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import com.android.aviro.data.api.AuthService
-import com.android.aviro.data.entity.auth.AuthRequestDTO
-import com.android.aviro.data.entity.auth.AuthResponseDTO
+import com.android.aviro.data.entity.auth.SignRequestDTO
+import com.android.aviro.data.entity.auth.TokensRequestDTO
 import com.android.aviro.data.entity.base.BaseResponse
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 
-class AuthRemoteDataSourceImp @Inject constructor(
+class AuthDataSourceImp @Inject constructor(
     private val authService : AuthService
-) : AuthDataSource  {
+) : AuthDataSource {
 
 
-    override suspend fun getTokens(request: AuthRequestDTO) : Result<Any> {
-        val response = authService.getTokens(request)
-
+    override suspend fun getTokens(request: TokensRequestDTO) : Result<Any> {
+        val response =  authService.getTokens(request)
+        // 타입 변형
         response.onSuccess {
             val data = it.data
-            Log.d("data", "${data}")
+
             if (data != null) {
                 return Result.success(it.data)
             } else {
@@ -30,19 +25,31 @@ class AuthRemoteDataSourceImp @Inject constructor(
                 return Result.success(errorResponse)
             }
 
-        }.onFailure {
+        }
+        return Result.failure(IllegalStateException("알 수 없는 오류가 발생했습니다."))
+
+    }
+
+    override suspend fun sign(request: SignRequestDTO): Result<Any> {
+        val response =  authService.sign(request)
+        response.onSuccess {
+            val data = it.data
+            if (data != null) {
+                return Result.success(it.data)
+            } else {
+                val errorResponse = BaseResponse(it.statusCode, it.message.orEmpty())
+                return Result.success(errorResponse)
+            }
 
         }
         return Result.failure(IllegalStateException("알 수 없는 오류가 발생했습니다."))
     }
 
 
-    override suspend fun removeTokens() {
+    override suspend fun removeTokens(refresh_token : String) {
 
     }
 
-    override suspend fun getTokens() : String {
-        throw UnsupportedOperationException("getTokens() is not supported in AuthRemoteDataSource")
-    }
+
 
 }
