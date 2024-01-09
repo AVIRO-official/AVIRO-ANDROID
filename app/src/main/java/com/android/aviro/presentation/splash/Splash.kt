9bf09d17e5eeba99dee9709219bc9e9d912b5a5d
honor-sky <keung903@naver.com>
2024-01-09 12:12:22 +0900

    refact : 로그인, 회원가입 로직 수정

--- app/src/main/java/com/android/aviro/presentation/splash/Splash.kt
+++ app/src/main/java/com/android/aviro/presentation/splash/Splash.kt
@@ -1,59 +1,48 @@
-package com.android.aviro.presentation
+package com.android.aviro.presentation.splash
 
-import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.activity.viewModels
 import com.android.aviro.R
 import com.android.aviro.presentation.guide.Guide
+import com.android.aviro.presentation.home.Home
 import com.android.aviro.presentation.sign.Sign
+import com.android.aviro.presentation.sign.SignViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class Splash : AppCompatActivity() {
+
+    private val viewmodel: SplashViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         goToHomeOrGuideOrSignWithDelay()
-        /*
-        Handler().postDelayed(Runnable {
 
-            val prefs = getPreferences(Context.MODE_PRIVATE)
-            val firstRun = prefs.getBoolean("firstRun", true)
-
-            if (firstRun) {
-                // 처음 실행될 때의 작업 수행
-                startActivity(Intent(this, Guide::class.java))
-                // "처음 실행 여부"를 false로 변경
-                prefs.edit().putBoolean("firstRun", false).apply()
-
-            } else {
-                startActivity(Intent(this, Sign::class.java))
-            }
-
-            // 현재 액티비티 닫기
-            finish()
-        }, 3000)
-*/
     }
-
 
 
     fun goToHomeOrGuideOrSignWithDelay() {
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = if(isFirstVisitor) {
-                onboardingNavigator.intent(this)
-            } else if(viewmodel.isSignedUp()) {
-                homeNavigator.intent(this)
+            val intent = if(viewmodel.isFirstStartApp()) { // 앱 최초 실행인지 확인 필
+                Intent(this, Guide::class.java)
+            } else if(viewmodel.isSignIn()) {             // 자동로그인
+                Intent(this, Home::class.java)
+            } else { //로그인 필요
+                Intent(this, Sign::class.java)
             }
-            else {
-                signupNavigator.intent(this)
-            }
+
             startActivity(intent)
             finish()
-        }, DELAYED_MILLIS)
+        }, 3000)
     }
+
+
 
 
 }
--- app/src/main/java/com/android/aviro/presentation/splash/SplashViewModel.kt
+++ app/src/main/java/com/android/aviro/presentation/splash/SplashViewModel.kt
@@ -1,4 +1,56 @@
 package com.android.aviro.presentation.splash
 
-class SplashViewModel {
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Context.MODE_PRIVATE
+import android.util.Log
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.android.aviro.domain.repository.AuthRepository
+import com.android.aviro.domain.repository.MemberRepository
+import com.android.aviro.domain.usecase.auth.AutoSignInUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+
+@HiltViewModel
+class SplashViewModel @Inject constructor (
+    private val autoSignInUseCase: AutoSignInUseCase,
+    @ApplicationContext private val context: Context
+) : ViewModel() {
+
+
+    // 최초 앱실행인지 확인 -> sharedPref
+    fun isFirstStartApp() : Boolean {
+        val prefs = context.getSharedPreferences("first_visitor",MODE_PRIVATE)
+        val firstRun = prefs.getBoolean("firstRun", true) // 처음엔 default 값 출력
+
+        if (firstRun) {
+            // 처음 실행될 때의 작업 수행
+            // "처음 실행 여부"를 false로 변경
+            prefs.edit().putBoolean("firstRun", false).apply()
+        }
+        return firstRun
+    }
+
+
+
+    // 자동로그인
+    fun isSignIn(): Boolean {
+        var isSign = false
+        viewModelScope.launch() {
+            // 로그인 성공 여부 반환
+            isSign = autoSignInUseCase.invoke()
+
+        }
+
+        return isSign
+
+    }
+
+
 }
