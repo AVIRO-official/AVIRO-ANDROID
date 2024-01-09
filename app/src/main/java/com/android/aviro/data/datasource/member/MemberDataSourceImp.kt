f34889d5badd28690fb7320303396dbf41df73fc
honor-sky <keung903@naver.com>
2024-01-09 11:53:47 +0900

    feat : member 관련 데이터 소스 - 신규 회원생성, 닉네임 검사 api 호출

--- app/src/main/java/com/android/aviro/data/datasource/member/MemberDataSourceImp.kt
+++ app/src/main/java/com/android/aviro/data/datasource/member/MemberDataSourceImp.kt
@@ -1,4 +1,36 @@
 package com.android.aviro.data.datasource.member
 
-class MemberDataSourceImp {
-}
+import com.android.aviro.data.api.MemberService
+import com.android.aviro.data.entity.auth.SignResponseDTO
+import com.android.aviro.data.entity.base.BaseResponse
+import com.android.aviro.data.entity.base.DataBodyResponse
+import com.android.aviro.data.entity.member.NicknameEntity
+import com.android.aviro.data.entity.member.NicknameCheckResponse
+import com.android.aviro.domain.entity.MemberEntity
+import javax.inject.Inject
+
+class MemberDataSourceImp @Inject constructor (
+    private val memberService : MemberService
+): MemberDataSource {
+
+
+    suspend override fun checkNickname(nickname : NicknameEntity) : Result<NicknameCheckResponse> {
+        return memberService.checkNickname(nickname)
+    }
+
+    suspend override fun creatMember(newMember: MemberEntity): Result<Any> {
+        val response = memberService.createMember(newMember)
+        response.onSuccess {
+            val data = it.data
+
+            if (data != null) {
+                return Result.success(it.data)
+            } else {
+                val errorResponse = BaseResponse(it.statusCode, it.message.orEmpty())
+                return Result.success(errorResponse)
+            }
+
+        }
+        return Result.failure(IllegalStateException("알 수 없는 오류가 발생했습니다."))
+        }
+    }
