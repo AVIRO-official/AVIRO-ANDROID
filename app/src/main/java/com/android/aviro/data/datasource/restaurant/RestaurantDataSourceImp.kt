e971c4f8cd7dd635899411243741110c82264ae8
honor-sky <keung903@naver.com>
2024-01-12 15:31:26 +0900

    feat : 서버로부터 가게데이터 불러오는 데이터소스, 가게데이터를 관리하는 Realm Local DB 데이터소스 생성

--- app/src/main/java/com/android/aviro/data/datasource/restaurant/RestaurantDataSourceImp.kt
+++ app/src/main/java/com/android/aviro/data/datasource/restaurant/RestaurantDataSourceImp.kt
@@ -1,4 +1,26 @@
 package com.android.aviro.data.datasource.restaurant
 
-class RestaurantDataSourceImp {
-}
+import com.android.aviro.data.api.RestaurantService
+import com.android.aviro.data.entity.base.BaseResponse
+import com.android.aviro.data.entity.restaurant.ReataurantReponseDTO
+import com.android.aviro.data.entity.restaurant.RestaurantRequestDTO
+import javax.inject.Inject
+
+class RestaurantDataSourceImp @Inject constructor (
+    private val restaurantService: RestaurantService
+) : RestaurantDataSource {
+
+    override suspend fun getRestaurant(request : RestaurantRequestDTO) : Result<ReataurantReponseDTO> {
+        val response = restaurantService.getRestaurant(request.x,request.y,request.wide,request.time)
+        response.onSuccess {
+            val data = it.data
+            if (data != null) {
+                return Result.success(it.data)
+            }
+
+        }.onFailure {
+            return Result.failure(IllegalStateException("알 수 없는 오류가 발생했습니다."))
+        }
+        return Result.failure(IllegalStateException("알 수 없는 오류가 발생했습니다."))
+        }
+    }
--- app/src/main/java/com/android/aviro/data/datasource/restaurant/RestaurantLocalDataSource.kt
+++ app/src/main/java/com/android/aviro/data/datasource/restaurant/RestaurantLocalDataSource.kt
@@ -1,4 +1,17 @@
 package com.android.aviro.data.datasource.restaurant
 
-class RestaurantLocalDataSource {
+import com.android.aviro.data.entity.restaurant.LocOfRestaurant
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.query.RealmResults
+
+interface RestaurantLocalDataSource {
+
+    fun saveRestaurant(RestaurantList : List<LocOfRestaurant>)
+    fun updateRestaurant()
+    fun deleteRestaurant()
+    fun getRestaurant() : List<LocOfRestaurant>
+    fun closeRealm()
+
+
 }
