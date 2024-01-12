6e37d06e9c824e590fa2f5eda145388b5f26b978
honor-sky <keung903@naver.com>
2024-01-12 15:09:27 +0900

    feat : 맵기능 - 권한설정, 위치추적, 가게 데이터 요청 및 마커 생성 코드 작성

--- app/src/main/java/com/android/aviro/presentation/home/Home.kt
+++ app/src/main/java/com/android/aviro/presentation/home/Home.kt
@@ -1,29 +1,43 @@
 package com.android.aviro.presentation.home
 
+import android.Manifest
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import android.content.Context.LOCATION_SERVICE
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.android.aviro.R
 import com.android.aviro.databinding.ActivityHomeBinding
+import com.android.aviro.presentation.home.ui.map.MapViewModel
+import com.android.aviro.presentation.sign.SignViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class Home : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_home)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_mypage, R.id.navigation_map, R.id.navigation_register
@@ -31,5 +45,7 @@ class Home : AppCompatActivity() {
         )
         //navView.setupWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
+
 }
--- app/src/main/java/com/android/aviro/presentation/home/ui/map/Map.kt
+++ app/src/main/java/com/android/aviro/presentation/home/ui/map/Map.kt
@@ -1,113 +1,218 @@
 package com.android.aviro.presentation.home.ui.map
 
-import android.content.om.OverlayManagerTransaction.newInstance
+import android.Manifest
+import android.content.Context.LOCATION_SERVICE
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
-import androidx.annotation.Nullable
-import androidx.core.content.ContentProviderCompat.requireContext
+import android.widget.Toast
+import androidx.annotation.UiThread
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.hilt.navigation.fragment.hiltNavGraphViewModels
-import androidx.lifecycle.ViewModelProvider
 import com.android.aviro.BuildConfig.NAVER_CLIENT_ID
 import com.android.aviro.R
-import com.android.aviro.data.datasource.datastore.DataStoreDataSourceImp_Factory.newInstance
 import com.android.aviro.databinding.FragmentMapBinding
-import com.android.aviro.databinding.FragmentRegisterBinding
-import com.android.aviro.presentation.sign.SignViewModel
-import com.naver.maps.map.MapFragment.newInstance
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.NaverMapSdk
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.overlay.OverlayImage
+import com.naver.maps.map.util.FusedLocationSource
 
 
-
-class MapFragment : Fragment(), OnMapReadyCallback {
+class Map : Fragment(), OnMapReadyCallback {
 
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
-    //private val viewmodel: MapViewModel by viewModels()
-    //private val viewmodel: MapViewModel by activityViewModels()
     private val viewmodel: MapViewModel by hiltNavGraphViewModels(R.id.navigation_map)
+    private lateinit var locationSource: FusedLocationSource
 
-    //private lateinit var mapView: MapView
+    val permission_list = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+    )
+    val GPS_ENABLE_REQUEST_CODE = 1000
+    val PERMISSIONS_REQUEST_CODE = 2000
+    val LOCATION_PERMISSION_REQUEST_CODE = 3000
+
 
         override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
         ): View {
-
             _binding = FragmentMapBinding.inflate(inflater, container, false)
             val root: View = binding.root
-
-            //viewmodel = ViewModelProvider(this).get(MapViewModel::class.java)
-            //viewmodel = MapViewModel()
-            //binding.viewmodel = viewmodel
-            //binding.lifecycleOwner = this
+            binding.viewmodel = viewmodel
+            binding.lifecycleOwner = this
 
             NaverMapSdk.getInstance(requireContext()).client =
-                NaverMapSdk.NaverCloudPlatformClient("${NAVER_CLIENT_ID}")
+                NaverMapSdk.NaverCloudPlatformClient(NAVER_CLIENT_ID)
 
-
-
-            var mapFragment = childFragmentManager.findFragmentById(R.id.map_fragment) as MapFragment
-            if (mapFragment == null) {
-                mapFragment = MapFragment()
-                childFragmentManager.beginTransaction().add(R.id.map_fragment, mapFragment).commit();
-            }
-            mapFragment.
-
-
-            viewmodel.isFirstStartMap()
+            checkPermission()
 
             return root
         }
 
+    fun initMap() {
 
-    override fun onStart() {
-        super.onStart()
-        binding.mapFragment.onStart()
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map_fragment, it).commit()
+            }
+
+        locationSource =
+            FusedLocationSource(this, PERMISSIONS_REQUEST_CODE)
+
+        mapFragment.getMapAsync(this)
     }
 
-    override fun onResume() {
-        super.onResume()
-        binding.mapFragment.onResume()
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+
+        val locationManager = context!!.getSystemService(LOCATION_SERVICE) as LocationManager
+
+        if (ActivityCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+             ActivityCompat.checkSelfPermission(context!!, Manifest.permission.ACCESS_COARSE_LOCATION)  != PackageManager.PERMISSION_GRANTED ) {
+
+            val currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            val cameraCurrentPosition = CameraPosition(LatLng(currentLocation!!.longitude, currentLocation.latitude),16.0)
+            naverMap.setCameraPosition(cameraCurrentPosition)
+        }
+
+
+        naverMap.uiSettings.isZoomControlEnabled = false
+
+        // 위치 추적 설정
+        naverMap.locationSource = locationSource
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+
+        // 현재 위치로 돌아오기
+        binding.locationFloatingButton.setOnClickListener {
+            naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        }
+
+        viewmodel.drawMarker()
+
+        viewmodel.markerList.observe(this, androidx.lifecycle.Observer {
+
+            Log.d("markerList","${it.markerList}")
+            it.markerList.forEach { makerEntity ->
+                val marker = makerEntity.marker as Marker
+                marker.map = naverMap
+            }
+
+
+        })
+
+
     }
 
-    override fun onPause() {
-        super.onPause()
-        binding.mapFragment.onPause()
+
+    fun checkPermission() {
+        if (checkOnOffGPS()) {
+            if (context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.ACCESS_FINE_LOCATION) } != PackageManager.PERMISSION_GRANTED &&
+                context?.let { ActivityCompat.checkSelfPermission(it, Manifest.permission.ACCESS_COARSE_LOCATION) } != PackageManager.PERMISSION_GRANTED ) {
+                // 이전에 퍼미션 거부한 적이 있는 경우
+                if (activity?.let { ActivityCompat.shouldShowRequestPermissionRationale(it, permission_list[0]) } == true) {
+                    Toast.makeText(context, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show()
+                }
+                requestPermissions(permission_list, 0)
+            } else {
+                // 권한 허용
+                initMap()
+            }
+        } else {
+            setGPS()
+        }
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        binding.mapFragment.onSaveInstanceState(outState)
+    // GPS가 켜져있는지 확인
+    private fun checkOnOffGPS(): Boolean {
+        val locationManager = context?.getSystemService(LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
     }
 
-    override fun onStop() {
-        super.onStop()
-        binding.mapFragment.onStop()
+    fun setGPS() {
+            val builder = AlertDialog.Builder(context!!)
+            builder.setTitle("GPS 비활성화")
+            builder.setMessage("GPS를 켜져 있어야 비건맵 서비스를 이용할 수 있습니다.".trimIndent())
+            builder.setCancelable(true)
+            builder.setPositiveButton("설정") { dialog, id ->
+                val callGPSSettingIntent =
+                    Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE)
+            }
+            builder.setNegativeButton(
+                "취소"
+            ) { dialog, id -> dialog.cancel() }
+            builder.create().show()
+
     }
+
+    // GPS 권한 설정 콜백
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            GPS_ENABLE_REQUEST_CODE ->
+                //사용자가 GPS 활성 시켰는지 검사
+                checkPermission()
+        }
+    }
+
+    // 위치 권한 설정 콜백
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode) {
+            PERMISSIONS_REQUEST_CODE -> {
+                var isCheck = true
+                for (isGranted in grantResults) {
+                    if (isGranted == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(context, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show()
+                        // 권한 거절시 다시 물어봄
+                        val builder = AlertDialog.Builder(context!!)
+                        builder.setMessage("퍼미션이 거부되었습니다.\n맵 서비스를 사용하시려면 앱 설정에서 위치 권한을 허용해주세요.")
+                        builder.setPositiveButton("확인") { dialog, which ->
+                        }
+                        builder.show()
+
+                        isCheck = false
+
+                        break
+                    }
+                }
+
+                if(isCheck) {
+
+
+                }
+            }
+
+
+        }
+
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
-        binding.mapFragment.onDestroy()
         _binding = null
     }
 
-    override fun onLowMemory() {
-        super.onLowMemory()
-        binding.mapFragment.onLowMemory()
-    }
 
-
-    override fun onMapReady(p0: NaverMap) {
-        TODO("Not yet implemented")
-    }
 }
