db400b5fefe86a4cb956c23073f3e338a8718d2a
honor-sky <keung903@naver.com>
2024-01-12 15:13:21 +0900

    feat : 가게데이터 요청, 마커 표시 유스케이스 기능 구현

--- app/src/main/java/com/android/aviro/domain/usecase/retaurant/GetRestaurantUseCase.kt
+++ app/src/main/java/com/android/aviro/domain/usecase/retaurant/GetRestaurantUseCase.kt
@@ -1,4 +1,24 @@
 package com.android.aviro.domain.usecase.retaurant
 
-class GetRetaurantUseCase {
+import android.content.Context
+import com.android.aviro.data.entity.marker.MarkerEntity
+import com.android.aviro.data.entity.marker.MarkerListEntity
+import com.android.aviro.data.entity.restaurant.RestaurantRequestDTO
+import com.android.aviro.domain.repository.MarkerRepository
+import com.android.aviro.domain.repository.RestaurantRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class GetRestaurantUseCase @Inject constructor (
+    private val restaurantRepository: RestaurantRepository,
+    private val markerRepository: MarkerRepository
+){
+
+    operator suspend fun invoke(x : String, y : String, wide : String, time : String)  {
+       return  restaurantRepository.getRestaurant(request = RestaurantRequestDTO(x, y, wide, time))
+    }
+
+    fun getMarker() : MarkerListEntity {
+        return markerRepository.getMarker()
+    }
 }
