eaa93c9de43a72caa531fa4ac6d68ede27eaa2c9
honor-sky <keung903@naver.com>
2024-01-12 15:25:00 +0900

    feat : 가게데이터 호출해 Local DB에 저장(empty면 모두 요청해 저장하고 그 이후엔 업데이트된 데이터만 수정), 마커 객체 생성 후 내부 저장소(캐시)에 저장

--- app/src/main/java/com/android/aviro/domain/repository/MarkerRepository.kt
+++ app/src/main/java/com/android/aviro/domain/repository/MarkerRepository.kt
@@ -1,4 +1,10 @@
 package com.android.aviro.domain.repository
 
+import com.android.aviro.data.entity.marker.MarkerEntity
+import com.android.aviro.data.entity.marker.MarkerListEntity
+import com.android.aviro.domain.entity.MemberEntity
+
 interface MarkerRepository {
+
+    fun getMarker() : MarkerListEntity
 }
--- app/src/main/java/com/android/aviro/domain/repository/RestaurantRepository.kt
+++ app/src/main/java/com/android/aviro/domain/repository/RestaurantRepository.kt
@@ -1,4 +1,9 @@
 package com.android.aviro.domain.repository
 
+import com.android.aviro.data.entity.marker.MarkerEntity
+import com.android.aviro.data.entity.restaurant.RestaurantRequestDTO
+
 interface RestaurantRepository {
+
+    suspend fun getRestaurant(request : RestaurantRequestDTO)
 }
