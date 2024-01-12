d9380564101830230cb7ffd3555c510d945e3f2c
honor-sky <keung903@naver.com>
2024-01-12 15:27:59 +0900

    feat : restaurant, marker Repository에 대한 의존성/ realm DB, 캐시 DataSource에 대한 의존성 / 가게데이터 요청 api 의존성 추가

--- app/src/main/java/com/android/aviro/data/di/RepositoryModule.kt
+++ app/src/main/java/com/android/aviro/data/di/RepositoryModule.kt
@@ -1,9 +1,13 @@
 package com.android.aviro.data.di
 
 import com.android.aviro.data.repository.AuthRepositoryImp
+import com.android.aviro.data.repository.MarkerRepositoryImp
 import com.android.aviro.data.repository.MemberRepositoryImp
+import com.android.aviro.data.repository.RestaurantRepositoryImp
 import com.android.aviro.domain.repository.AuthRepository
+import com.android.aviro.domain.repository.MarkerRepository
 import com.android.aviro.domain.repository.MemberRepository
+import com.android.aviro.domain.repository.RestaurantRepository
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
@@ -29,6 +33,19 @@ abstract class RepositoryModule {
     abstract fun bindsMemberRepository(
        member_repository: MemberRepositoryImp,
     ): MemberRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindsRestaurantRepository(
+        restaurant_repository: RestaurantRepositoryImp,
+    ): RestaurantRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindsMarkerRepository(
+        marker_repository: MarkerRepositoryImp,
+    ): MarkerRepository
+
 
 
 }
