791af1133b55b4d810aeceda7dd00a3d8f33a75c
honor-sky <keung903@naver.com>
2024-01-12 15:28:37 +0900

    feat : 가게데이터 요청 api 추가

--- app/src/main/java/com/android/aviro/data/api/RestaurantService.kt
+++ app/src/main/java/com/android/aviro/data/api/RestaurantService.kt
@@ -1,4 +1,23 @@
 package com.android.aviro.data.api
 
-class RestaurantService {
+import com.android.aviro.data.entity.auth.TokensRequestDTO
+import com.android.aviro.data.entity.auth.TokensResponseDTO
+import com.android.aviro.data.entity.base.DataBodyResponse
+import com.android.aviro.data.entity.restaurant.ReataurantReponseDTO
+import com.android.aviro.data.entity.restaurant.RestaurantRequestDTO
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+
+interface RestaurantService {
+    @GET("map")
+    suspend fun getRestaurant(
+        @Query("x") x : String,
+        @Query("y") y : String,
+        @Query("wide") wide : String,
+        @Query("time") time : String
+
+    ): Result<DataBodyResponse<ReataurantReponseDTO>>
 }
