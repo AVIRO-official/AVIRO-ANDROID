98d198b9c7216623db60ac3bb5ab7b520ce55eb2
honor-sky <keung903@naver.com>
2024-01-12 15:29:52 +0900

    feat : 네이버 마커 객체 생성 후 관리하는 내부저장소 생성

--- app/src/main/java/com/android/aviro/data/datasource/marker/MarkerCacheDataSource.kt
+++ app/src/main/java/com/android/aviro/data/datasource/marker/MarkerCacheDataSource.kt
@@ -1,11 +1,13 @@
 package com.android.aviro.data.datasource.marker
 
+import com.android.aviro.data.entity.marker.MarkerEntity
+import com.android.aviro.data.entity.marker.MarkerListEntity
 import com.naver.maps.map.overlay.Marker
 
-interface MarkerLocalDataSource {
+interface MarkerCacheDataSource {
 
-    fun saveMarker(placeId : String, marker : Marker)
-    fun getMarker()
+    fun saveMarker(custom_marker : List<MarkerEntity>)
+    fun getMarker() : MarkerListEntity
     fun updateMarker()
 
 }
