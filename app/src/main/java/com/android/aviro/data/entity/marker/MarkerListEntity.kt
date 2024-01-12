932c22f869245c4ee7ecbd5b7bc54dd4cb2411b5
honor-sky <keung903@naver.com>
2024-01-12 15:34:38 +0900

    feat : 네이버 지도 마커 담는 entity 생성(마커를 매번 생성하지 않고 이 entity를 캐싱해 사용)

--- app/src/main/java/com/android/aviro/data/entity/marker/MarkerListEntity.kt
+++ app/src/main/java/com/android/aviro/data/entity/marker/MarkerListEntity.kt
@@ -1,4 +1,13 @@
 package com.android.aviro.data.entity.marker
 
-class MarkerListEntity {
-}
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.Serializer
+
+@Serializable
+data class MarkerListEntity(
+    @SerializedName("markerList")
+    val markerList : List<MarkerEntity>
+)
