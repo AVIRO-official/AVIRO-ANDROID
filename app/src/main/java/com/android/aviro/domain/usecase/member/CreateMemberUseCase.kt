e021cace2e9e488c1c0a5a8876add97a78b483ff
honor-sky <keung903@naver.com>
2024-01-28 03:10:40 +0900

    feat : 멤버 생성과 관련된 유스케이스 -> 닉네임 검사(회원가입 생성시 사용), 가입 성공시 회원정보 저장

--- app/src/main/java/com/android/aviro/domain/usecase/member/CreateMemberUseCase.kt
+++ app/src/main/java/com/android/aviro/domain/usecase/member/CreateMemberUseCase.kt
@@ -1,5 +1,8 @@
 package com.android.aviro.domain.usecase.member
 
+import android.util.Log
+import com.android.aviro.data.entity.auth.SignResponseDTO
+import com.android.aviro.data.entity.base.MappingResult
 import com.android.aviro.data.entity.member.NicknameEntity
 import com.android.aviro.data.entity.member.NicknameCheckResponse
 import com.android.aviro.data.repository.MemberRepositoryImp
@@ -10,15 +13,27 @@ class CreateMemberUseCase @Inject constructor (
     private val memberRepository: MemberRepositoryImp
     ) {
 
-    suspend operator fun invoke(nickname: String, birth : Int?, gender : String?, marketingAgree : Int) : Result<Any> {
-        return memberRepository.creatMember(nickname, birth, gender, marketingAgree)
+    suspend operator fun invoke(nickname: String, birth : Int?, gender : String?, marketingAgree : Int) : MappingResult  {
+        val response = memberRepository.creatMember(nickname, birth, gender, marketingAgree)
+        when(response){
+            is MappingResult.Success<*> -> {
+                response.let {
+                    val data = it.data as SignResponseDTO
+                    Log.d("Member","새로운 멤버 정보 저장 : ${data}")
+                    memberRepository.saveMemberInfoToLocal("user_id",data.userId)
+                    memberRepository.saveMemberInfoToLocal("user_name",data.userName)
+                    memberRepository.saveMemberInfoToLocal("user_email",data.userEmail)
+                    memberRepository.saveMemberInfoToLocal("nickname",data.nickname)
+                }
+            }
+            is MappingResult.Error -> {}
+        }
+        return response
 
     }
 
     suspend fun checkNickname(nickname : String) : Result<NicknameCheckResponse> {
         return memberRepository.checkNickname(NicknameEntity(nickname))
-
     }
-
 
 }
