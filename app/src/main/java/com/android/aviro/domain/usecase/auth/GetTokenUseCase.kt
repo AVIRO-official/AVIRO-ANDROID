275a3b65ed450ee703e8fa934fdb03b69dd32ef5
honor-sky <keung903@naver.com>
2024-01-28 03:07:01 +0900

    feat : 내부저장소의 토큰 가져오기(굳이 유스케이스로 만들 필요가 있는지 다시 확인)

--- app/src/main/java/com/android/aviro/domain/usecase/auth/GetTokenUseCase.kt
+++ app/src/main/java/com/android/aviro/domain/usecase/auth/GetTokenUseCase.kt
@@ -1,4 +1,15 @@
 package com.android.aviro.domain.usecase.auth
 
-class GetTokenUseCase {
+import com.android.aviro.domain.repository.AuthRepository
+import javax.inject.Inject
+
+class GetTokenUseCase@Inject constructor (
+    private val authRepository: AuthRepository
+) {
+    suspend operator fun invoke() : String? {
+        val token = authRepository.getTokensFromLocal()
+        //if(token != null) return
+        return token
+
+    }
 }
