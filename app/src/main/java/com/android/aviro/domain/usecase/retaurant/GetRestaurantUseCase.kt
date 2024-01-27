02af81e3d73624e32cdad33da0a92170455a5fa5
honor-sky <keung903@naver.com>
2024-01-28 03:09:11 +0900

    feat : 가게정보 가져오는 유스케이스 -> 가게 위치 정보 요청과 마커 정보 요청 2가지로 추가 분리

--- app/src/main/java/com/android/aviro/domain/usecase/retaurant/GetRestaurantUseCase.kt
+++ app/src/main/java/com/android/aviro/domain/usecase/retaurant/GetRestaurantUseCase.kt
@@ -1,25 +1,27 @@
 package com.android.aviro.domain.usecase.retaurant
 
-import android.content.Context
+import com.android.aviro.data.entity.base.MappingResult
 import com.android.aviro.data.entity.marker.MarkerEntity
 import com.android.aviro.data.entity.marker.MarkerListEntity
+import com.android.aviro.data.entity.restaurant.ReataurantReponseDTO
 import com.android.aviro.data.entity.restaurant.RestaurantRequestDTO
-import com.android.aviro.domain.repository.MarkerRepository
 import com.android.aviro.domain.repository.RestaurantRepository
-import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
 class GetRestaurantUseCase @Inject constructor (
-    private val restaurantRepository: RestaurantRepository,
-    private val markerRepository: MarkerRepository
+    private val restaurantRepository: RestaurantRepository
 ){
 
-    operator suspend fun invoke(x : String, y : String, wide : String, time : String)  {
-       return  restaurantRepository.getRestaurant(request = RestaurantRequestDTO(x, y, wide, time))
+    // 가게 위치 데이터 업데이트 하기 -> 에러 처리
+    operator suspend fun invoke(x : String, y : String, wide : String, time : String, isInitMap : Boolean): MappingResult {
+        // 서버 데이터와 동기화 -> 마커 업데이트 -> 마커 가져옴 (사용자 입장에서는 가게 데이터 가져온 것)
+       return restaurantRepository.getRestaurantLoc(x, y, wide, time, isInitMap)
     }
 
-    fun getMarker() : MarkerListEntity {
-        return markerRepository.getMarker()
+    // 마커 데이터 가져오기 -> 데이터 그려주기
+    fun getMarker(isInitMap : Boolean, reataurant_list : ReataurantReponseDTO) : List<MarkerEntity>? {
+        return restaurantRepository.getMarker(isInitMap, reataurant_list)
     }
+
 
 }
