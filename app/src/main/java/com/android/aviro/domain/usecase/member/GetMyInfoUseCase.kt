3ecdcfa7f6f79c002dab961c55735e20a5270006
honor-sky <keung903@naver.com>
2024-01-29 09:38:29 +0900

    feat : 챌린지 화면-유저 정보 불러오기(닉네임, 활동기록,챌린지 레벨) 유스케이스 및 레포지토리 추가

--- app/src/main/java/com/android/aviro/domain/usecase/member/GetMyInfoUseCase.kt
+++ app/src/main/java/com/android/aviro/domain/usecase/member/GetMyInfoUseCase.kt
@@ -1,5 +1,8 @@
 package com.android.aviro.domain.usecase.member
 
+import com.android.aviro.data.entity.base.DataBodyResponse
+import com.android.aviro.data.entity.member.MyInfoCountResponse
+import com.android.aviro.data.entity.member.MyInfoLevelResponse
 import com.android.aviro.domain.repository.MemberRepository
 import com.android.aviro.domain.usecase.retaurant.GetRestaurantUseCase
 import javax.inject.Inject
@@ -9,16 +12,23 @@ class GetMyInfoUseCase  @Inject constructor (
 ) {
 
     // 사용자 기본 정보
-    suspend fun getNickname() {
+    suspend fun getNickname() : String? {
        val nickname = memberRepository.getMemberInfoFromLocal("nickname")
+        return nickname
 
     }
 
     // 사용자 가게 등록 정보 (활동 정보)
-    suspend fun getCount() {
+    suspend fun getCount() : Result<DataBodyResponse<MyInfoCountResponse>> {
         val userId = memberRepository.getMemberInfoFromLocal("user_id")
+        return memberRepository.getCount(userId!!)
 
     }
 
     // 사용자 챌린지 정보 (활동 정보)
+    suspend fun getChallengeLevel() : Result<MyInfoLevelResponse> {
+        val userId = memberRepository.getMemberInfoFromLocal("user_id")
+        return memberRepository.getChallengeLevel(userId!!)
+
+    }
 }
