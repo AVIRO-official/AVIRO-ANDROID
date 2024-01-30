89603f49d5dd56a7ed6832117b72d5f73d6bce5e
honor-sky <keung903@naver.com>
2024-01-30 16:23:48 +0900

    feat : 검색 기능 개발 - 카카오 검색 api 연결, 검색 결과 리사이클러뷰 로직 개발, 검색바/튜토리얼/최근검색어 이벤트 수정

--- app/src/main/java/com/android/aviro/presentation/search/Search.kt
+++ app/src/main/java/com/android/aviro/presentation/search/Search.kt
@@ -1,21 +1,19 @@
 package com.android.aviro.presentation.search
 
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
-import com.android.aviro.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.aviro.data.entity.restaurant.SearchEntity
 import com.android.aviro.databinding.ActivitySearchBinding
-import com.android.aviro.presentation.sign.SignViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class Search : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySearchBinding
+    private lateinit var binding:ActivitySearchBinding
 
     private val viewmodel: SearchViewModel by viewModels()
 
@@ -27,27 +25,31 @@ class Search : AppCompatActivity() {
         binding.viewmodel = viewmodel
         binding.lifecycleOwner = this
 
+        binding.searchRecyclerview.adapter = SearchAdapter()
+
         binding.backBtn.setOnClickListener {
-            //overridePendingTransition(R.anim.slide_left_enter, R.anim.slide_right_enter)
-            finish()
+           finish()
         }
 
 
-        // 검색중 아님 -> 이전검색어 있는지 여부에 따라 다른 내용
-        viewmodel.isPreSearched.observe(this, Observer {
-            val addLayout = if(it) layoutInflater.inflate(R.layout.search_word, null) else layoutInflater.inflate(R.layout.search_tutorial, null)
-            binding.includeLayout.addView(addLayout)
-
-        })
-
-
-        viewmodel.isSearching.observe(this, Observer {
-            // 검색중 아니면 포커스 내려가게
-            if(it) binding.EditTextSearchBar.requestFocus()  else binding.EditTextSearchBar.clearFocus()
-            // 검색중이면 리스트 화면
-            //val addLayout = if(it) layoutInflater.inflate(R.layout.searched_restaurant_item, null) else layoutInflater.inflate(R.layout.search_tutorial, null)
-            //binding.includeLayout.addView(addLayout)
-        })
-
     }
 }
+
+
+
+
+// 검색중 아님 -> 이전검색어 있는지 여부에 따라 다른 내용
+/*
+viewmodel.isPreSearched.observe(this, Observer {
+    val addLayout = if(it) layoutInflater.inflate(R.layout.search_word, null) else layoutInflater.inflate(R.layout.search_tutorial, null)
+    //addLayout.viewmodel = viewmodel
+    binding.includeLayout.addView(addLayout)
+
+    // 이전검색어 모두 삭제
+
+    // 새로운 검색어 추가될때마다 업데이트
+})
+ */
+
+
+
--- app/src/main/java/com/android/aviro/presentation/search/SearchViewModel.kt
+++ app/src/main/java/com/android/aviro/presentation/search/SearchViewModel.kt
@@ -14,20 +14,25 @@ import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.android.aviro.R
+import com.android.aviro.data.entity.restaurant.SearchEntity
+import com.android.aviro.domain.usecase.retaurant.SearchRestaurantUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
+    private val searchRestaurantUseCase: SearchRestaurantUseCase,
     @ApplicationContext private val context: Context
 ) : ViewModel() {
 
     // 내부저장소에서 이전에 검색했던 검색어 추출
     val prefs = context.getSharedPreferences("pre_searched_data", Context.MODE_PRIVATE)
-    val searchedSet = prefs.getStringSet("searchedSet", setOf()) // 처음엔 default 값 출력
+    val searchedSet = mutableSetOf<String>() // 처음엔 default 값 출력
 
     private val _isSearching = MutableLiveData<Boolean>()
     val isSearching: LiveData<Boolean> = _isSearching
@@ -35,40 +40,107 @@ class SearchViewModel @Inject constructor(
     private val _isPreSearched = MutableLiveData<Boolean>()
     val isPreSearched: LiveData<Boolean> = _isPreSearched
 
+    private val _SearchedPlaceList = MutableLiveData<List<SearchEntity>>() // 가게 리스트
+    var SearchedPlaceList : LiveData<List<SearchEntity>> = _SearchedPlaceList
+
+    private var searchedKeyword  = ""
+
     init {
         _isSearching.value = false //검색중 아님
-        _isPreSearched.value = searchedSet!!.size != 0
+        _isPreSearched.value = !prefs.all.isEmpty()
+        _SearchedPlaceList.value =  listOf(
+            SearchEntity("","우리마트","3","121","123","","","",
+                "이촌동","","url","010")
+        )
+    }
+
+    fun initList() {
+        viewModelScope.launch {
+            Log.d("keyword","${searchedKeyword}")
+            val response = searchRestaurantUseCase.getSearchedRestaurantList(
+                searchedKeyword,
+                "127.03596593951177",
+                "37.5071726244927",
+                1,
+                15,
+                "accuracy"
+            )
+            Log.d("response","${response}")
+            response.onSuccess {
+                Log.d("response","${it.documents}")
+                _SearchedPlaceList.value = it.documents
+            }
+        }
+        /*
+        // 응답 결과는 메타 데이터까지 모두 갖고 있는 형태
+        searchRestaurantUseCase.
+        repository.list { plantList ->
+            _plantList.postValue(plantList)
+        }
+         */
+    }
+
+    // usecase 응답 결과의 isEnd == false 일 경우, 다음 리스트 불러와 합침
+    fun nextList() {
+        /*
+        val currentPlantList = plantList.value ?: return
+        repository.next(currentPlantList) { palntList ->
+            // 기존 list와 다음 list를 더해줍니다.
+            val mergedPlants = currenPlantList.plants.toMutableList()
+                .apply { addAll(plantList.plants) }
+            plantList.plants = mergedPlants
+            _plantList.postValue(plantList)
+        }
+
+         */
     }
 
     // 실제 remote 통해 검색한 경우에만 저장
     fun storeSearchedWord(search_word : String) {
-        searchedSet!!.add(search_word)
+        //searchedSet!!.add(search_word)
+        searchedSet.add(search_word)
         prefs.edit().putStringSet("searchedSet", searchedSet).apply()
         _isPreSearched.value = true
     }
 
 
-    // 검색창 클릭하면 바로 활성화
-    fun onEditTextFocusChanged(editText : View, hasFocus : Boolean) {
-        //Log.d("hasFocus","${hasFocus}")
+    // 포커스 여부에 따라 키보드
+    fun onEditTextFocusChanged(editTextView : View, hasFocus : Boolean) {
+        Log.d("hasFocus","${hasFocus}")
         _isSearching.value = hasFocus
         val inputMethodManager = context.getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-        if(hasFocus) inputMethodManager.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT) else inputMethodManager.hideSoftInputFromWindow(editText.windowToken, 0)
+        if(hasFocus) inputMethodManager.showSoftInput(editTextView, InputMethodManager.SHOW_IMPLICIT) else inputMethodManager.hideSoftInputFromWindow(editTextView.windowToken, 0)
+        if(hasFocus) editTextView.requestFocus()  else editTextView.clearFocus()
 
     }
 
 
     // 검색어가 editText 에 들어욤
-    fun searchRestaurant(editText : EditText) {
-        val keyword = editText.text
-        // remote 검색 요청
+    fun searchRestaurant(view: View, keyCode: Int, event: KeyEvent) : Boolean {
+        val edit_text = view as EditText
+        val keyword = edit_text.text.toString()
+        // 엔터 누르면 검색
+        if (keyCode == KeyEvent.KEYCODE_ENTER && event.action == KeyEvent.ACTION_DOWN) {
+            // Enter 키를 눌렀을 때 수행할 작업
+            // 예: 키보드 숨김
+            val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+            // remote 검색 요청
+            searchedKeyword = keyword
+            storeSearchedWord(keyword)
+            initList()
+            return true
+        }
+        return false
 
 
     }
 
     // 검색바 뒤로가기
-    fun onClickBack() {
-        _isSearching.value = false
+    fun onClickBack(view: View) {
+        //_isSearching.value = false
+        val editText = view.rootView.findViewById<EditText>(R.id.EditTextSearchBar)
+        onEditTextFocusChanged(editText,false)
 
     }
 
