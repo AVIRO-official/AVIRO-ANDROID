45a9c25fb59bb551b948469ad25243f865ff18b6
honor-sky <keung903@naver.com>
2024-02-02 01:06:48 +0900

    feat : 검색결과로 보여줄 최종 entity 추가, 검색결과의 비건타입을 요청하는 유스케이스와 데이터로직 추가

--- app/src/main/java/com/android/aviro/domain/repository/RestaurantRepository.kt
+++ app/src/main/java/com/android/aviro/domain/repository/RestaurantRepository.kt
@@ -2,16 +2,15 @@ package com.android.aviro.domain.repository
 
 import com.android.aviro.data.entity.base.MappingResult
 import com.android.aviro.data.entity.marker.MarkerEntity
-import com.android.aviro.data.entity.restaurant.ReataurantReponseDTO
-import com.android.aviro.data.entity.restaurant.RestaurantRequestDTO
-import com.android.aviro.data.entity.restaurant.SearchRestaurantResponse
+import com.android.aviro.data.entity.restaurant.*
+import com.android.aviro.domain.entity.SearchedRestaurantItem
 
 interface RestaurantRepository {
 
     suspend fun getRestaurantLoc(x : String, y : String, wide : String, time : String, isInitMap : Boolean) : MappingResult
     fun getMarker(isInitMap : Boolean, reataurant_list : ReataurantReponseDTO) : List<MarkerEntity>?
     suspend fun getRestaurantDetail()
-    suspend fun searchRestaurant(keyword : String, x : String, y : String, page : Int, size : Int, sort : String) : Result<SearchRestaurantResponse>
-
+    suspend fun searchRestaurant(keyword : String, x : String, y : String, page : Int, size : Int, sort : String) : Result<SearchedPlaceListResponse>
+    suspend fun getVeganTypeOfSearching(request : List<Document>) : Result<List<SearchedRestaurantItem>>
 
 }
--- app/src/main/java/com/android/aviro/domain/usecase/retaurant/SearchRestaurantUseCase.kt
+++ app/src/main/java/com/android/aviro/domain/usecase/retaurant/SearchRestaurantUseCase.kt
@@ -1,13 +1,19 @@
 package com.android.aviro.domain.usecase.retaurant
 
-import com.android.aviro.data.entity.restaurant.SearchRestaurantResponse
+import com.android.aviro.data.entity.restaurant.*
+import com.android.aviro.domain.entity.SearchedRestaurantItem
 import com.android.aviro.domain.repository.RestaurantRepository
 import javax.inject.Inject
 
 class SearchRestaurantUseCase @Inject constructor (
     private val restaurantRepository: RestaurantRepository
 ){
-    suspend fun getSearchedRestaurantList(keyword : String, x : String, y : String, page : Int, size : Int, sort : String) : Result<SearchRestaurantResponse> {
+    suspend fun getSearchedRestaurantList(keyword : String, x : String, y : String, page : Int, size : Int, sort : String) : Result<SearchedPlaceListResponse> {
         return restaurantRepository.searchRestaurant(keyword, x, y, page, size, sort)
+    }
+
+    suspend fun getSearchedRestaurantVeganList(SearchedPlaceRawList : List<Document>) : Result<List<SearchedRestaurantItem>> {
+        return restaurantRepository.getVeganTypeOfSearching(SearchedPlaceRawList)
+
     }
 }
