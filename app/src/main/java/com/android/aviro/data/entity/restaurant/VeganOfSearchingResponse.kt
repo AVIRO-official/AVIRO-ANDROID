339af7552dc96e63921a852702c3eb815865a389
honor-sky <keung903@naver.com>
2024-02-02 01:03:10 +0900

    feat : 검색화면 리사이클러뷰 레이아웃 위치 변경, 검색결과 비건 타입 아이콘 바인딩 어댑터 설정

---
+++ app/src/main/java/com/android/aviro/data/entity/restaurant/VeganOfSearchingResponse.kt
@@ -0,0 +1,26 @@
+package com.android.aviro.data.entity.restaurant
+
+import com.google.gson.annotations.SerializedName
+
+data class VeganTypeResponse (
+
+        @SerializedName("statusCode")
+        val statusCode : Int,
+        @SerializedName("body")
+        val body : List<VeganType>,
+
+
+        )
+
+data class VeganType(
+        @SerializedName("index")
+        val index : Int,
+        @SerializedName("placeId")
+        val placeId : String,
+        @SerializedName("allVegan")
+        val allVegan : Boolean,
+        @SerializedName("someMenuVegan")
+        val someMenuVegan : Boolean,
+        @SerializedName("ifRequestVegan")
+        val ifRequestVegan : Boolean
+)
---
+++ app/src/main/java/com/android/aviro/domain/entity/SearchedRestaurantItem.kt
@@ -0,0 +1,3 @@
+package com.android.aviro.domain.entity
+
+data class SearchedRestaurantItem()
