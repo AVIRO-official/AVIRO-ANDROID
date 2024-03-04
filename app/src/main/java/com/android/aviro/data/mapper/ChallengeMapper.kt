99d882a5034ae2245118a73a071aca75357e9fd6
honor-sky <keung903@naver.com>
2024-03-05 01:26:00 +0900

    feat : 데이터 계층의 model 데이터를 도메인 계층의 entity 데이터로 변환, 사용자 기능에 꼭 필요한 데이터만 넘김

--- app/src/main/java/com/android/aviro/data/mapper/ChallengeMapper.kt
+++ app/src/main/java/com/android/aviro/data/mapper/ChallengeMapper.kt
@@ -1,4 +1,19 @@
 package com.android.aviro.data.mapper
 
-class ChallengeMapper {
+import com.android.aviro.data.model.challenge.ChallengeCommentResponse
+import com.android.aviro.data.model.challenge.ChallengeInfoResponse
+import com.android.aviro.domain.entity.challenge.ChallengeComment
+import com.android.aviro.domain.entity.challenge.ChallengeInfo
+
+fun ChallengeInfoResponse.toChallengeInfo() : ChallengeInfo {
+    return ChallengeInfo(
+        period = this.period,
+        title = this.title
+    )
+}
+
+fun ChallengeCommentResponse.toChallengeComment() : ChallengeComment {
+    return ChallengeComment(
+        comment = this.comment
+    )
 }
